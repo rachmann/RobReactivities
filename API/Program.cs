c8716d40dfe1ba4f19e7c29892310c8be15ee8d9
Robert A <achmann@outlook.com>
2025-05-10 19:51:24 -0300

    initial API

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,42 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
-
-///////////////////////////////////////////////
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt =>
+internal class Program
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-///////////////////////////////////////////////
-// Configure the HTTP request pipeline.
-var app = builder.Build();
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.MapControllers();
+        ///////////////////////////////////////////////
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+        ///////////////////////////////////////////////
+        // Configure the HTTP request pipeline.
+        var app = builder.Build();
 
-app.Run();
+        app.MapControllers();
+
+        // Need to use Service Locator pattern to get the DbContext
+        using var scope = app.Services.CreateScope();
+
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<AppDbContext>();
+            await context.Database.MigrateAsync();
+            // Seed the database with initial data 
+            await DbInitualizer.SeedEmptyDb(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred during migration");
+        }
+
+        app.Run();
+    }
+}
--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,19 +1,28 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain;
 
 public class Activity
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [MaxLength(100)]
     public required string Title { get; set; }
 
-    public DateTime Date { get; set; } = new DateTime(1900, 1, 1);
+    public DateTime Date { get; set; } = new DateTime(1753, 1, 1);
 
+    [MaxLength(2000)]
+    public required string Description { get; set; }
+
+    [MaxLength(100)]
     public required string Category { get; set; }
     public bool IsCancelled { get; set; }
 
+    [MaxLength(200)]
     public required string City { get; set; }
 
+    [MaxLength(1500)]
     public required string Venue { get; set; }
 
     public double Latitude { get; set; }
