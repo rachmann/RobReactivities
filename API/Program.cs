c9b2a6d63093f25c4bb2fca8545aa59296d4004c
Robert A <achmann@outlook.com>
2025-05-10 15:08:23 -0300

    Added NuGet packages and DbContext

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 ///////////////////////////////////////////////
 // Add services to the container.
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 ///////////////////////////////////////////////
 // Configure the HTTP request pipeline.
 var app = builder.Build();
